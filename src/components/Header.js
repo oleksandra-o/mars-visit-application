fa6248b136d050edc183ac02a7b58479ee09651c
oleksandra-o <156704678+oleksandra-o@users.noreply.github.com>
2024-08-29 16:15:31 -0700

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Header() {
+  return (
+    <header className="app-header">
+      <h1>Mars Mission Application Form</h1>
+    </header>
+  );
+}
+
+export default Header;

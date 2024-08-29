89186347510fe46b60d2b6a4e69698bcf0857e52
oleksandra-o <156704678+oleksandra-o@users.noreply.github.com>
2024-08-29 16:15:27 -0700

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer className="app-footer">
+      <p>&copy; 2024 Mars Mission. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
